0d696ba7865f01809fc94766bce6200e2cd6b151
ff6347 <hash@ff6347.email>
2023-09-26 17:01:22 +0200

    refactor: Static express server

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ const app = express();
 const helmet = require("helmet");
 const cors = require("cors");
 const path = require("path");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.use(helmet());
 app.use(cors());
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.resolve(__dirname, "../build")));
 app.use("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../build", "index.html"));
 });
 // catch all
 //app.get("*", (req, res) => {
